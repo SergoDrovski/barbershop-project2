7a1d82e0daa98dab1ea494a0ddb5c2e86e6872ca
Сергей <sergiodrovski@gmail.com>
2021-03-20 17:48:23 +0300

    --

--- js/slideJS.js
+++ js/slideJS.js
@@ -1,17 +1,43 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlidesTwo(slideIndex);
+showSlidesOne(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex = slideIndex + n);
+  showSlidesTwo(slideIndex = slideIndex + n);
+}
+
+function currentSlideOne(n) {
+  showSlidesOne(slideIndex = n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlidesTwo(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlidesOne(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlidesOne");
+  var dots = document.getElementsByClassName("slider--toggle--advantages");
+
+  if(n > slides.length) {
+    slideIndex = 1;
+  }
+  if(n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "off");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className = dots[slideIndex-1].className.replace("off", "active");
+}
+
+function showSlidesTwo(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("mySlidesTwo");
     var dots = document.getElementsByClassName("slider--toggle--review");
 
     if(n > slides.length) {
@@ -24,8 +50,8 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "off");
+        dots[i].className = dots[i].className.replace("activeTwo", "off");
     }
     slides[slideIndex-1].style.display = "flex";
-    dots[slideIndex-1].className = dots[slideIndex-1].className.replace("off", "active");
+    dots[slideIndex-1].className = dots[slideIndex-1].className.replace("off", "activeTwo");
 }
